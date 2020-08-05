376e32e90653028514c890b5a031e7f8887b3e66
vkarki <54143475+vkarki@users.noreply.github.com>
2020-08-05 13:22:21 +0530

    Update provider.tf
    Removed the subscription ID from azure rm

--- terraform/orchestrate/rightSize/provider.tf
+++ terraform/orchestrate/rightSize/provider.tf
@@ -1,6 +1,5 @@
 
 provider "azurerm" {
-  subscription_id = "9166f63a-fcb8-40b6-8b20-aa1803f52fe9"
   tenant_id       = "c28b2d80-129e-4693-88bd-5f43e59072f9"
   version         = "=2.19.0"
   features {}
